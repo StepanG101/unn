f98a038ca775a5ac53d6ef4ad0c1b98856f9fc23
StepanG101 <sgargoma1@gmail.com>
2026-09-21 19:13:21 +0300

    Update config.js

--- PythonProject_PetLife/petlife-mvp/js/config.js
+++ PythonProject_PetLife/petlife-mvp/js/config.js
@@ -1,5 +1,5 @@
 window.PETLIFE_CONFIG = {
-  OPENWEATHER_API_KEY: "efc0a615631e09bccb604199eb97d713",
+  OPENWEATHER_API_KEY: "",
   OPENROUTER_API_KEY: "YOUR_OPENROUTER_API_KEY",
   OPENROUTER_MODEL: "meta-llama/llama-3.2-11b-vision-instruct:free"
 };
